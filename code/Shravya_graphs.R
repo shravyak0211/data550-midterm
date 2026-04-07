9b6d56b34fb9b469cd6ad555ea2795e98e5526ac
Shravya Kanchanapally <shravyak0211@gmail.com>
2026-04-07 14:01:43 -0400

    Add Shravya graphs

---
+++ code/Shravya_graphs.R
@@ -0,0 +1,51 @@
+library(ggplot2)
+
+nba_stats <- read.csv("data_clean/nba_combined.csv")
+
+# 100+ minute players
+nba_stats <- nba_stats[nba_stats$mins_played >= 100, ]
+
+
+#Element 1: Box plots per position
+nba_stats$position <- factor(nba_stats$position, levels = c("PG", "SG", "SF", "PF", "C"))
+
+e1 <- ggplot(nba_stats, aes(x = position, y = pts, fill = position)) +
+  geom_boxplot(outlier.shape = 21, outlier.fill = "white") +
+  labs(
+    title = "Points Scored per Game per 36 Minutes by Position",
+    x = "Position (Pos)",
+    y = "Points Scored (PTS)"
+  ) +
+  theme_bw() +
+  theme(legend.position = "none")
+
+ggsave("output/box_plot_1.png", plot = e1, width = 8, height = 6, dpi = 150) 
+
+
+#Element 2: Scattetrplot
+e2 <- ggplot(nba_stats, aes(x = age, y = mins_played)) +
+  geom_point(alpha = 0.6, color = "steelblue") +
+  geom_smooth(method = "lm", se = TRUE, color = "darkred") +
+  labs(
+    title = "Age vs. Minutes Played per 36 Minutes",
+    x = "Age (Age)",
+    y = "Minutes Played (MP)"
+  ) +
+  theme_bw()
+
+ggsave("output/scatter_plot_2.png", plot = e2, width = 8, height = 6, dpi = 150)
+
+
+#Element 3: Scatterplot
+e3 <- ggplot(nba_stats, aes(x = games, y = rebounds_defensive)) +
+  geom_point(alpha = 0.6, color = "steelblue") +
+  geom_smooth(method = "lm", se = TRUE, color = "darkred") +
+  labs(
+    title = "Games vs. Defensive Rebounds per 36 Minutes",
+    x = "Games Played (G)",
+    y = "Defensive Rebounds (DRB)"
+  ) +
+  theme_bw()
+
+ggsave("output/scatter_plot_3.png", plot = e3, width = 8, height = 6, dpi = 150)
+
